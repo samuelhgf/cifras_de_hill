cd04653db158e06fcbdf9cc2be4892d47c0df91e
Samuel Franca <samuelhenriques2000@gmail.com>
2019-11-13 13:43:20 -0300

    Change some variable names and ajust identation

--- encrypt.py
+++ encrypt.py
@@ -2,36 +2,37 @@ matrix_length = int(input("Digite o tamanho da matriz: "))
 
 code_matrix = []
 
-alfabeto = ['Z']
+alphabet = ['Z']
 for x in range(65, 90):
-    alfabeto.append(chr(x))
+    alphabet.append(chr(x))
 
 for i in range(matrix_length):
-    code_matrix.append(list(map(int, input("Digite a linha %d separada por espaços entre os numeros: " % (i+1)).split(" "))))
+    code_matrix.append(
+        list(map(int, input("Digite a linha %d separada por espaços entre os numeros: " % (i + 1)).split(" "))))
 
 original_message = input("Digite a mensagem original: ").upper()
 
-if (len(original_message) > matrix_length):
-    while(len(original_message) % matrix_length != 0):
-          original_message += original_message[len(original_message) - 1]
-elif(len(original_message) < matrix_length):
-    while(matrix_length % len(original_message) != 0):
-          original_message += original_message[len(original_message) - 1]
+if len(original_message) > matrix_length:
+    while len(original_message) % matrix_length != 0:
+        original_message += original_message[len(original_message) - 1]
+elif len(original_message) < matrix_length:
+    while matrix_length % len(original_message) != 0:
+        original_message += original_message[len(original_message) - 1]
 
-message_map = []                                         
+message_map = []
 
 counter = 0
 for i in range(int(len(original_message) / matrix_length)):
     column = []
-    for i in range(matrix_length):
-        column.append(alfabeto.index(original_message[counter]))
+    for j in range(matrix_length):
+        column.append(alphabet.index(original_message[counter]))
         counter += 1
-        
+
     message_map.append(column)
 
 matrix_num_crip = []
 
-cripted_message = ""
+encrypted_message = ""
 
 line_counter = 0
 for i in range(int(len(original_message) / matrix_length)):
@@ -43,10 +44,9 @@ for i in range(int(len(original_message) / matrix_length)):
             sum_element_with_letter += (element * message_map[line_counter][element_counter])
             element_counter += 1
 
-        cripted_message += alfabeto[sum_element_with_letter % 26]
+        encrypted_message += alphabet[sum_element_with_letter % 26]
 
     line_counter += 1
     matrix_num_crip.append(result_column)
 
-
-print(cripted_message)
+print(encrypted_message)
